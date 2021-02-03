70f605b8ee5849aefc23882d8c0367c8ba0a0631
jigsaw-netizen <gijsbarmentlo@gmail.com>
2021-02-03 12:12:35 +0100

    ALL OK, push before changing train test split to per patient split and  concatenant after to guartantee train and test have similar proportions of data from each patient

--- model v3_all patients.py
+++ model v3_all patients.py
@@ -94,6 +94,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 # Create basic classifiers
 
+def evaluate_classifiers(classifiers, X_train, y_train, X_test, y_test):
+    
+    
+    return 
+
 basic_clf = ExtraTreesClassifier()
 basic_clf.fit(X_train, y_train)
 
@@ -133,61 +138,85 @@ from sklearn.model_selection import StratifiedKFold
 # from multiprocessing import Pool
 
 # Create model 
-extra_trees_clf = ExtraTreesClassifier()
+extra_trees_clf = ExtraTreesClassifier(n_jobs = -1)
 
 # Optimise hyperparameters
 
 scorer_object = make_scorer(roc_auc_score, greater_is_better = True, needs_proba = True)
 
 distributions = dict(criterion = ['gini', 'entropy'],
-                     n_estimators = range(10, 1000), 
-                     max_depth = [1, 10, 100, 1000, None],
-                     min_samples_split = range(2, 50),
-                     min_samples_leaf = range(1, 200),
+                     n_estimators = range(200, 8000), 
+                     max_depth = [50, 100, 150, 200, 1000, None],
+                     min_samples_split = range(20, 100),
+                     min_samples_leaf = range(10, 200),
                      max_features = ['auto', 'sqrt', 'log2'],
-                     max_leaf_nodes = [10, 100, 1000, None],
-                     min_impurity_decrease = [0, 0.01, 0.25, 1, 10],
+                     max_leaf_nodes = [10, 100, 500, 1000, None],
+#                   min_impurity_decrease = [0, 0.01, 0.25, 1, 10],
                      bootstrap = [False, True],
                      class_weight = ['balanced', 'balanced_subsample', None],
-                     ccp_alpha = np.linspace(0, 0.013, 13) ,
-                     max_samples = np.linspace(0.001, 0.999, 20).astype('float'))
+                     ccp_alpha = np.linspace(0, 0.08, 20) ,
+                     max_samples = np.linspace(0.001, 0.5, 20).astype('float'))
 
 # pool = Pool(4)
 
 clf = EvolutionaryAlgorithmSearchCV(extra_trees_clf, 
                                     distributions, 
-                                    population_size = 50,
+                                    population_size = 100,
                                     gene_mutation_prob = 0.10,
                                     gene_crossover_prob = 0.5,
                                     tournament_size = 3,
-                                    generations_number = 5,
-                                    cv = StratifiedKFold(n_splits = 3),
+                                    generations_number = 6,
+                                    cv = StratifiedKFold(n_splits = 5),
                                     verbose = 1,
                                     scoring = scorer_object,
                                     n_jobs = 1)
                                     #pmap = pool.map
 
-search = clf.fit(X_train, y_train)
-print(f'best parameters found in search are {search.best_params_}')
+search = clf.fit(X_train, y_train) # TODO test removing search
+
+## best parameters found in search are {'criterion': 'entropy', 'n_estimators': 321,
+# 'max_depth': 100, 'min_samples_split': 42, 'min_samples_leaf': 17, 'max_features': 'sqrt',
+# 'max_leaf_nodes': 1000, 'min_impurity_decrease': 0, 'bootstrap': False, 
+# 'class_weight': 'balanced_subsample', 'ccp_alpha': 0.004333333333333333, 
+# 'max_samples': 0.10605263157894737}
+
+#second run gave Best individual is: {'criterion': 'entropy', 'n_estimators': 956, 'max_depth': 150, 
+# 'min_samples_split': 68, 'min_samples_leaf': 55, 'max_features': 'auto', 
+# 'max_leaf_nodes': 1000, 'bootstrap': False, 'class_weight': 'balanced_subsample', 
+# 'ccp_alpha': 0.05052631578947368, 'max_samples': 0.4737368421052632}
+#   with fitness: 0.8395730353160524 ; {'auc': 0.8227768361581921, 'f1score': 0.5006485084306096, 'accuracy': 0.32019449161622426, 'balanced_accuracy': 0.540361581920904, 'recall': 0.772}
+#%%
+
+print(f'best parameters found in search are {clf.best_params_}')
 
 #%% save and evaluate
 
 #best_clf = ExtraTreesClassifier(n_estimators = 837, max_depth = 38, min_samples_leaf = 1, min_impurity_decrease = 0, criterion = 'entropy', bootstrap = False, min_samples_split = 2, class_weight = 'balanced')
 
-best_clf = ExtraTreesClassifier(**clf.best_params_)
-best_clf = best_clf.fit(X_train, y_train)
+#best_clf = ExtraTreesClassifier(**clf.best_params_)
+#best_clf = best_clf.fit(X_train, y_train)
+
+best_clf = clf.best_estimator_
 metrics_dict = compute_metrics(best_clf, X_test, y_test)
 print(f'model trained on all patients has following performance metrics after hyperparam tuning: {metrics_dict}')
 
 import pickle
-filename = 'neurovista_model_allpat_2feb_hyperparam_genetic_long.sav'
+filename = 'neurovista_model_allpat_2feb_hyperparam_genetic_long_v2.sav'
 pickle.dump(best_clf, open(filename, 'wb'))
 
 #%% evaluate
-import pickle
 
-with open(r"neurovista_model_pat3_31jan_hyperparam_genetic.sav", "rb") as input_file:
-    best_clf = pickle.load(input_file)
+from sklearn.model_selection import cross_val_score, KFold
+
+paper_model = ExtraTreesClassifier(n_estimators=5000, random_state=0, max_depth=15, n_jobs=2,criterion='entropy', min_samples_split=7)
+# gets 0.84 avg auc with 10fold, lowest fold 0.61 then 0.78
+cv_auc = cross_val_score(paper_model, X, np.ravel(y), scoring = scorer_object, cv = KFold(10))
+print(cv_auc.sum()/10)
+
+
+#%%
+#with open(r"neurovista_model_pat3_31jan_hyperparam_genetic.sav", "rb") as input_file:
+#    best_clf = pickle.load(input_file)
 
 y_pred_proba = best_clf.predict_proba(X_test)[:, [1]]
 
